fb8817a080d18f6828f8d1a0c9b33f44032abf8b
Tom Berry <tberry860@gmail.com>
2013-03-21 12:20:25 -0700

    added django-storages

--- main/settings.py
+++ main/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
