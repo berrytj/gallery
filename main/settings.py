0d115439e8503b1fa7b9c6b4d7800058cbff833f
Tom Berry <tberry860@gmail.com>
2013-03-21 12:17:13 -0700

    added s3 support

--- main/settings.py
+++ main/settings.py
@@ -55,11 +55,15 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
+import os.path
+PROJECT_DIR = os.path.dirname(__file__)
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/Tom/Desktop/gallery/main/production_statics'
+STATIC_ROOT = os.path.join(PROJECT_DIR, "production_statics")
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,8 +71,16 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/Users/Tom/Desktop/gallery/main/statics',
+    os.path.join(PROJECT_DIR, "statics"),
 )
+
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = 'AKIAJ7YKYP5FVFMAMUYQ'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_STORAGE_BUCKET_NAME = 'drgallery'
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = S3_URL
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -100,7 +112,7 @@ ROOT_URLCONF = 'main.urls'
 WSGI_APPLICATION = 'main.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/Tom/Desktop/gallery/main/templates',
+    os.path.join(PROJECT_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
@@ -110,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 )
 
 # A sample logging configuration. The only tangible logging
